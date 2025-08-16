56031d4780f27301d016639fff49a8c40140e7e1
sakib <smsakib2001@gmail.com>
2025-08-17 00:16:31 +0600

    feat: Add avatar URL to user response in 'me' endpoint

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -179,7 +179,17 @@ class AuthController extends Controller
     )]
     public function me(Request $request)
     {
-        return response()->json($request->user());
+        $user = $request->user();
+        
+        // Add full URL for avatar
+        $userArray = $user->toArray();
+        if ($user->avatar) {
+            $userArray['avatar_url'] = asset('storage/' . $user->avatar);
+        } else {
+            $userArray['avatar_url'] = null;
+        }
+
+        return response()->json($userArray);
     }
 
     #[OA\Post(
