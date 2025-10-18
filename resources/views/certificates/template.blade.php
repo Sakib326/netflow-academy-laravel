1368107ae8c766a06eee0ce02a45b7091c5be531
sakib <smsakib2001@gmail.com>
2025-10-18 17:16:43 +0600

    Implement code changes to enhance functionality and improve performance

--- resources/views/certificates/template.blade.php
+++ resources/views/certificates/template.blade.php
@@ -1,47 +1,84 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>Certificate of Completion</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Certificate</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pinyon+Script&display=swap" rel="stylesheet">
+
     <style>
         body {
-            font-family: 'Helvetica', sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .certificate-container {
+            width: 100%;
             text-align: center;
-            border: 20px solid #787878;
-            padding: 50px;
-            height: 550px;
         }
 
-        h1 {
-            font-size: 50px;
-            color: #333;
+        .certificate-inner {
+            display: inline-block;
+            width: 800px;
+            text-align: center;
+            margin: auto;
+            position: relative;
         }
 
-        h2 {
-            font-size: 30px;
+        .certificate-inner img {
+            width: 800px;
         }
 
-        p {
-            font-size: 20px;
+        h1.name {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -110%);
+            font-size: 55px;
+            font-weight: 500;
+            letter-spacing: 2px;
+            color: #252525;
+            white-space: nowrap;
+            text-transform: capitalize !important;
+            font-family: 'Pinyon Script', cursive;
         }
 
-        .date,
-        .code {
-            font-size: 16px;
-            color: #555;
-            margin-top: 40px;
+        p.body-text {
+            position: absolute;
+            top: 58%;
+            left: 50%;
+            transform: translate(-50%, -70%);
+            color: #252525;
+            font-weight: 400;
+            width: 600px;
+            font-family: 'Lato', sans-serif;
+            font-size: 18px;
+            line-height: 1.4;
         }
     </style>
 </head>
 
 <body>
-    <p>This is to certify that</p>
-    <h1>{{ $userName }}</h1>
-    <p>has successfully completed the course</p>
-    <h2>{{ $courseName }}</h2>
-    <p class="date">Issued on: {{ $issueDate }}</p>
-    <p class="code">Certificate Code: {{ $certificateCode }}</p>
+    <div class="certificate-container">
+        <div class="certificate-inner">
+            <img src="./certificate-image.png" alt="certificate background">
+
+            <!-- Name -->
+            <h1 class="name">{{ $userName }}</h1>
+
+            <!-- Body / description text -->
+            <!-- Body / description text -->
+            <p class="body-text">
+                This certificate is awarded to {{ $userName }} for successfully finishing NetFlow Academy's
+                "{{ $courseName }}" course, gaining essential skills for professional development.
+                Issued on {{ $issueDate }}. Certificate Code: {{ $certificateCode }}.
+            </p>
+        </div>
+    </div>
 </body>
 
 </html>
